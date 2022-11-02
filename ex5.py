814ce2655160595df9763a2d479a6b147edf5e82
Bruno Gião <66087393+greybrunix@users.noreply.github.com>
2022-11-02 16:54:58 +0000

    cenas

--- ex5.py
+++ ex5.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-def conv_csm_json(content, headers):
+def conv_csm_json(content, headers, flags):
     # SECTION 1 - Preamble
     new = re.sub(r'([A-Za-z ]*)\n',r'\1',content)
     new = re.split(r',',new); # NOTE separates content by commas
@@ -14,9 +14,10 @@ def conv_csm_json(content, headers):
     # A for does not provide enough control
     while i < (len(tmp_head)-1):
         if tmp_head[i] == tmp_head[i+1]: # NOTE we found a list
-            flag = 1; test = str(headers[i]); tmp_array[headers[i]] = []
+            flag = 1; test = str(tmp_head[i]); tmp_array[test] = []
         while flag == 1:
             if test not in tmp_head:
+                # NOTE that there must be spaces in varying size lists, TRIVIAL implementation
                 # This is due to removing from tmp_head
                 flag = 0
                 new.insert(i,tmp_array[test])
@@ -67,33 +68,43 @@ def main():
     # SECTION 2 - parsing lists
     lines[0] = re.sub(r'([A-Za-z ]*)\n', r'\1', lines[0])
     headers = lines.pop(0)
-    tst = re.findall(r'([A-Za-z0-9 \_\-]+){([0-9]+)(,[0-9]+)?}(::[A-Z]+)?',headers) 
+    tst = re.findall(r'([A-Za-z0-9 \_\-]+){([0-9]+)(,([0-9]+))?}(::[A-Z]+)?',headers) 
     matches_list = []
+    flags = {}
     if tst:
         for x in tst:
-            headers = re.subn(r'(?<=,)(?=,)|(?<=},)(?=,)|(?<=,,)(?=)',
-                            x.group(1),
+            print(x)
+            headers = re.sub(r'(?<=\,)(?=\,)|(?<=}\,)(?=\,)|(?<=\,\,)(?=)',
+                            x[0],
                             headers,
-                            count=int(x.group(2))
+                            count=int(x[3])
                             )
-            matches_list.append(x)
-        headers = re.split(r',', headers[0])
+            x = list(x)
+            flags[x[0]] = []
+            if x[2]:
+                flags[x[0]].append(x[1])
+            if x[-1]:
+                flags[x[0]].append(x[-1])
+            x.insert(1,'{');x.insert(4,'}')
+            x.pop(5)
+            matches_list.append(''.join(x))
+        headers = re.split(r'(?<!{\d),(?!\d})', headers)
         for x in matches_list:
-            headers.remove(x.group())
+            headers.remove(x)
     else:
         headers = re.split(r',', headers)
     # NOTE considering only one list can be accepted
     # TODO maybe use findall to get all instances of list creations
     final = lines.pop(len(lines)-1)
-
+    print(flags)
 
     # SECTION 3 - Creating a buffer with JSON file and writing to it
     with open(file_name+'.json', 'w+') as f:
         f.write('[\n')
         for line in lines:
-            js_object = "\t{\n\t"; js_object += conv_csm_json(line, headers)
+            js_object = "\t{\n\t"; js_object += conv_csm_json(line, headers, flags)
             js_object += "},\n"; f.write(js_object)
-        js_object = "\t{\n\t"; js_object += conv_csm_json(final, headers)
+        js_object = "\t{\n\t"; js_object += conv_csm_json(final, headers, flags)
         js_object += "}\n]"; f.write(js_object); f.close()
     return 0
 
@@ -105,3 +116,4 @@ if __name__ == '__main__':
 # NOTE already accepting base case, one single list in any position
 # TODO multiple lists, lists with varying length and functions over lists
 # TODO MAYBE subs
+
